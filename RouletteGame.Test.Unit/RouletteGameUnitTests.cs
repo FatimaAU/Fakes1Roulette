22504179b9dd4b43fa2020da853b1411467ec587
FatimaAU <fatima_kodro@hotmail.com>
2018-02-21 13:20:43 +0100

    Roulette so far

---
+++ RouletteGame.Test.Unit/RouletteGameUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using RouletteGame;
+using NUnit.Framework;
+namespace RouletteGame.Test.Unit
+{
+    [TestFixture]
+    public class RouletteGameUnitTests
+    {
+
+        //private Legacy.RouletteGame _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            //_uut = new Calculator();
+        }
+
+
+    }
+}
